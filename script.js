878b3e54a6e952de07f3a89204b80b8f91889520
CoolONEOfficial <coolone.official@gmail.com>
2019-02-06 00:39:04 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+
+// $('.grid').masonry({
+//     // options...
+//     itemSelector: '.grid-item',
+//     columnWidth: 200
+// });
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
