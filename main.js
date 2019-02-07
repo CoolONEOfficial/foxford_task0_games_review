202d47c87aeb467fce2d7aced97e46364eb6380c
CoolONEOfficial <coolone.official@gmail.com>
2019-02-08 02:18:15 +0300

    Started platforms integration

--- main.js
+++ main.js
@@ -16,7 +16,16 @@ Vue.component(
         props: ['item'],
         template: `
 <div class="my-section">
-    <h1 class="display-4">{{ item.title }}</h1>
+    <div class="row">
+        <div class="col">
+            <h1 class="display-4">{{ item.title }}</h1>
+        </div>
+        <div class="col">
+            <i :class="'fab fa-' + key"
+            v-for="[key, val] in item.platforms"
+            :key="val"></i>
+        </div>
+    </div>
     <div class="row">
         <div class="col-lg-6 col-md-12">
             <p class="lead text-justify">{{ item.description }}</p>
@@ -57,6 +66,17 @@ Vue.component(
 new Vue({
     el: '#app',
     data: {
+        platformsEnum: Object.freeze({
+            "android": 1,
+            "ios": 2,
+            "pc": 3,
+            "macos": 4,
+            "linux": 5,
+            "windows": 6,
+            "xbox": 7,
+            "playstation": 8,
+            "nintendo-switch": 10,
+        }),
         intro: 'Программирование - неотъемлемая часть современного мира.Его изучение актуально на сегодняшний день. С помощью программирования пишутся коды и создаются программы, новые технологии. В него входит информатика, математика и даже английский язык. Предлагаю Вам просмотреть список игр-тренажеров для изучения программирования. Найдите ту программу, с которой  Вам будет удобно и легко получать новые знания!',
         items: [
             {
@@ -64,60 +84,145 @@ new Vue({
                 prefix: 'lb',
                 title: 'LightBot',
                 description: 'Каждый мечтал управлять роботом. Новые технологии - наше будущее. Lightbot дает Вам эту возможность. В этой игре вы должны запрограммировать робота, чтобы тот прошел конкретную дистанцию и зажжег все свето-платформы. Игра довольно лёгкая и затягивающая. Lightbot  - самый увлекательный способ для детей  научиться основам программирования и понять его логику.',
+                platforms: new Map(
+                    [
+                        [data.platformsEnum.android, "#"],
+                        [data.platformsEnum.ios, "#"],
+                        [data.platformsEnum.windows, "#"],
+                        [data.platformsEnum.macos, "#"],
+                    ]
+                ),
             },
             {
                 key: 1,
                 prefix: 'scr',
                 title: 'Scratch',
                 description: 'Scratch является понятным и доступным языком программирования для всех людей. В этой  программе можете научиться созданием  наброска собственных анимированных и интерактивных историй, игр и других проектов. Необязательно иметь опыт в этих сферах, достаточно иметь мотивацию и время. Творите, публикуйте, играйте!',
+                platforms: new Map(
+                    [
+                        [data.platformsEnum.android, "#"],
+                        [data.platformsEnum.ios, "#"],
+                        [data.platformsEnum.windows, "#"],
+                        [data.platformsEnum.macos, "#"],
+                        [data.platformsEnum.linux, "#"],
+                    ]
+                ),
             },
             {
                 key: 2,
                 prefix: 'cm',
                 title: 'CodeMonkey',
                 description: 'Занимательная видеоигра, где вы можете научиться без проблем основам программирования.  Решая интересные  головоломки, вы развиваете свое аналитическое мышление и приобретаете важный опыт для решения такого рода задач. Все просто: написать код, поймать бананы, спасти мир. Вы сможете сделать самостоятельно приложения и проверить свои способности. Всегда — учиться, все — знать! Удачи в познании новом!',
+                platforms: new Map(
+                    [
+                        [data.platformsEnum.android, "#"],
+                        [data.platformsEnum.ios, "#"],
+                        [data.platformsEnum.windows, "#"],
+                        [data.platformsEnum.macos, "#"],
+                    ]
+                ),
             },
             {
                 key: 3,
                 prefix: 'cco',
                 title: 'CodeCombat',
                 description: 'CodeCombat – это платформа для обучения нескольких языков программирования в игровой форме. Начиная работу в CodeCombat, Вы попадаете в магический мир и приобретаете способность колдовать, создавая код программы. С прохождением уровня Ваш персонаж будет получать необходимые вещи. При этом, чем качественней выполнена задача, тем лучше награда в конце. Собирайте самоцветы, побеждайте в схватках с врагами и станьте лучшим магом в программировании!',
+                platforms: new Map(
+                    [
+                        [data.platformsEnum.android, "#"],
+                        [data.platformsEnum.ios, "#"],
+                        [data.platformsEnum.windows, "#"],
+                        [data.platformsEnum.macos, "#"],
+                    ]
+                ),
             },
             {
                 key: 4,
                 prefix: 'sp',
                 title: 'Swift Playgrounds',
                 description: 'Swift Playgrounds – уникальное познавательное приложение от компании Apple. Ваша задача состоит в том, что  нужно запрограммировать существо на сбор кристаллов на языке программирования Swift или решить головоломку. Вы можете создать даже свою карту и пройти ее. Желаем Вам  в полном объеме насладиться результатом!',
+                platforms: new Map(
+                    [
+                        [data.platformsEnum.android, "#"],
+                        [data.platformsEnum.ios, "#"],
+                        [data.platformsEnum.windows, "#"],
+                        [data.platformsEnum.macos, "#"],
+                        [data.platformsEnum.linux, "#"],
+                    ]
+                ),
             },
             {
                 key: 5,
                 prefix: 'f',
                 title: 'Factorio',
                 description: 'В этой увлекательной игре вы, космонавт, потерпели крушение и пытаетесь выжить на невиданной  планете, имеющей богатую фауну и флору. Основная цель игры - построить ракету и отправиться домой. Вы можете создавать разные конструкции для строительства, находить всевозможные ресурсы, управлять заводами, однако Вам будет мешать сама природа. Будьте внимательны, просчитывайте стратегию, и вы обязательно пройдете игру!',
+                platforms: new Map(
+                    [
+                        [data.platformsEnum.windows, "#"],
+                        [data.platformsEnum.macos, "#"],
+                        [data.platformsEnum.linux, "#"],
+                    ]
+                ),
             },
             {
                 key: 6,
                 prefix: 'hrm',
                 title: 'Human Resource Machine',
                 description: 'Хотели ли Вы когда-нибудь взять контроль над группой офисных работников? Если да, то Human Resource Machine предоставит Вам эту возможность. Human Resource – это необычная и красочная игра в жанре инди – головоломки. Ничего сложного: решить задачу и перейти на новый уровень. Для этого вам нужно запрограммировать Ваших сотрудников на решение головоломки. Не бойтесь затруднений, в этой игре все сделано так, что разберется любой. Будьте хорошим руководителем и спасите свою работу, ведь машины грядут... чтобы лишить Вас имущества!',
+                platforms: new Map(
+                    [
+                        [data.platformsEnum.windows, "#"],
+                    ]
+                ),
             },
             {
                 key: 7,
                 prefix: 'mine',
                 title: 'Minecraft',
                 description: 'Minecraft - компьютерная игра в жанре выживания в открытом море, в котором много опасностей. Книга Крэйга Ричардсона «Программируй с Minecraft» поможет Вам Научиться и узнать больше о языке программирования Python с помощью погружения в игру Minecraft. Вас ждет приключение и время проведения, полного веселья. Программируете, играйте и наслаждайтесь своим творением!',
+                platforms: new Map(
+                    [
+                        [data.platformsEnum.android, "#"],
+                        [data.platformsEnum.ios, "#"],
+                        [data.platformsEnum.wp, "#"],
+                        [data.platformsEnum.windows, "#"],
+                        [data.platformsEnum.macos, "#"],
+                        [data.platformsEnum.linux, "#"],
+                        [data.platformsEnum.xbox360, "#"],
+                        [data.platformsEnum.xboxone, "#"],
+                        [data.platformsEnum.ps3, "#"],
+                        [data.platformsEnum.ps4, "#"],
+                        [data.platformsEnum.psvita, "#"],
+                        [data.platformsEnum.nswitch, "#"],
+                        [data.platformsEnum.wiiu, "#"],
+                    ]
+                ),
             },
             {
                 key: 8,
                 prefix: 'sh',
                 title: 'SHENZHEN I/O',
                 description: 'В такой замечательной и развивающей игре как SHENZHEN/IO вы можете побывать на месте рабочего в сфере программирования, работая на крупнейшую китайскую компанию по производству электроники. В вашем распоряжении все, что нужно для профессионального программиста. Вы будете решать интересные головоломки и продвигаться дальше. Основное решение задач  будет опираться на несложном, мощном языке ассемблера. Программируйте, начиная от плат, заканчивая ЖК-экранами. Вы всегда можете прочитать пособие, которое предоставляет Вам игра. Проявите творческий подход! \n',
+                platforms: new Map(
+                    [
+                        [data.platformsEnum.windows, "#"],
+                        [data.platformsEnum.linux, "#"],
+                        [data.platformsEnum.macos, "#"],
+                    ]
+                ),
             },
             {
                 key: 9,
                 prefix: 'hoc',
                 title: 'Hour of Code',
                 description: 'Hour of code предназначен для обучения программированию и информатики. Если Вы начнете работу в этом приложении, то приобретете много полезного в данных сферах. Вы можете учиться программировать, как и с тематикой "Star Wars", а может и "Minecraft", так и по теме мультфильма "Холодное Сердце".',
+                platforms: new Map(
+                    [
+                        [data.platformsEnum.windows, "#"],
+                        [data.platformsEnum.linux, "#"],
+                        [data.platformsEnum.macos, "#"],
+                    ]
+                ),
             },
         ]
     }
